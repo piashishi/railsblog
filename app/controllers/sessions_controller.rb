2e450b85e89517df2a42e67921ca50318278e461
piashishi <piashishi@qq.com>
2015-10-03 11:32:20 +0800

    add session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  	def new
+  	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])νΉτ€‰
+			log_in user
+			redirect_to user
+		else
+			flash[:danger] = 'Invalid email/password combination' ν°τ€ƒ‚τ€τ€…¦τ€† τ€€…τ€€τ€Έν―€τ€…µτ€ƒ½
+			ender 'new'
+		end
+	end
+
+	def destroy
+		log_out
+		redirect_to root_url
+	end
+
 end
+
