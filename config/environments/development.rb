0a1f8fa3c5c4369e41d72885e7fc8babe19bbee2
piashishi <piashishi@qq.com>
2017-02-22 03:08:01 -0500

    add forget/reset password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = '192.168.56.102:3000'
+  config.action_mailer.default_url_options = { host: host }
+
 end
