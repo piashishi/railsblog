0000d8fddd2afb01dba4452d3c20eb129d5ee5bb
piashishi <piashishi@qq.com>
2017-02-22 05:14:13 -0500

    add follow function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 	before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 	before_action :correct_user, only: [:edit, :update]
 	before_action :admin_user, only: :destroy
+	before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
 
 	def index
 		@users = User.paginate(page: params[:page])
@@ -10,6 +11,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@microposts = @user.microposts.paginate(page: params[:page])
 	end
 
 	def new
@@ -53,22 +55,30 @@ class UsersController < ApplicationController
      	end
      end
 
+     def following
+     	@title = "Following"
+     	@user = User.find(params[:id])
+     	@users = @user.following.paginate(page: params[:page])
+     	render 'show_follow'
+     end
+     def followers
+     	@title = "Followers"
+     	@user = User.find(params[:id])
+     	@users = @user.followers.paginate(page: params[:page])
+     	render 'show_follow'
+     end
+
      private
      def user_params
      	params.require(:user).permit(:name, :email, :password,
      		:password_confirmation)
      end
 
-     def logged_in_user
-     	unless logged_in?
-     		store_location
-     		flash[:danger] = "Please log in."
-     		redirect_to login_url
-     	end
-     end
 
      def correct_user
      	@user = User.find(params[:id])
      	redirect_to(root_url) unless current_user?(@user)
      end
+
+
  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-	root 		'static_pages#home'
+	root 'static_pages#home'
 	get 'help' => 'static_pages#help'
 	get 'about' => 'static_pages#about'
 	get 'contact' => 'static_pages#contact'
@@ -11,8 +7,13 @@ Rails.application.routes.draw do
 	get 'login' => 'sessions#new'
 	post 'login' => 'sessions#create'
 	delete 'logout' => 'sessions#destroy'
-
-	resources :users
+	resources :users do
+		member do
+			get :following, :followers
+		end
+	end
 	resources :account_activations, only: [:edit]
 	resources :password_resets, only: [:new, :create, :edit, :update]
+	resources :microposts, only: [:create, :destroy]
+	resources :relationships, only: [:create, :destroy]
 end
