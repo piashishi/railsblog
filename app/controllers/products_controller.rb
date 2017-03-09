c98e9eb94118f163e9928ebce4dd814b462a0c14
piashishi <piashishi@qq.com>
2017-03-09 04:06:28 -0500

    add product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,18 @@ class ProductsController < ApplicationController
      	redirect_to products_url
      end
 
+	def edit
+		@product = Product.find(params[:id])
+	end
+	def update
+		@product = Product.find(params[:id])
+		if @product.update_attributes(product_params)
+			flash[:success] = "product updated"
+			redirect_to products_path
+		else
+			render 'edit'
+		end
+	end
 private
 	def product_params
 		params.require(:product).permit(:price, :description, :ProductId, :image)
